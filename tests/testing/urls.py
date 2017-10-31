fba372fc52423798373463c0f6ddc789e4cfcca2
Maxim Maslin <zapzarap@yandex.ru>
2017-10-31 14:01:51 +0300

    update urls.py

--- tests/testing/urls.py
+++ tests/testing/urls.py
@@ -5,28 +5,24 @@ from . import views
 
 urlpatterns = [
 
+    # User
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^user/(?P<id>\d)/$', views.UserDetail.as_view()),
+
+    # QuestionSet
+    url(r'^questionsets/$', views.QuestionSetList.as_view()),
+    url(r'^questionset/(?P<id>\d)/$', views.QuestionSetDetail.as_view()),
+
     # Question
     url(r'^questions/$', views.QuestionList.as_view()),
     url(r'^question/(?P<id>\d)/$', views.QuestionDetail.as_view()),
 
-    # Answer
-    url(r'^answers/$', views.AnswerList.as_view()),
-    url(r'^answer/(?P<id>\d)/$', views.AnswerDetail.as_view()),
+    # Option
+    url(r'^options/$', views.OptionList.as_view()),
+    url(r'^option/(?P<id>\w)/$', views.OptionDetail.as_view()),
 
-    # Test
-    url(r'^tests/$', views.TestList.as_view()),
-    url(r'^test/(?P<id>\d)/$', views.TestDetail.as_view()),
-
-    # Author
-    url(r'^authors/$', views.AuthorList.as_view()),
-    url(r'^author/(?P<id>\w)/$', views.AuthorDetail.as_view()),
-
-    # Respondent
-    url(r'^respondents/$', views.RespondentList.as_view()),
-    url(r'^respondent/(?P<id>\w)/$', views.RespondentDetail.as_view()),
-
-    # Grade
-    url(r'^grades/$', views.GradeList.as_view()),
-    url(r'^grade/(?P<id>\d)/$', views.GradeDetail.as_view()),
+    # Submission
+    url(r'^submissions/$', views.SubmissionList.as_view()),
+    url(r'^submission/(?P<id>\w)/$', views.SubmissionDetail.as_view()),
 
 ]
