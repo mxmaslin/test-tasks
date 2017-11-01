415fb3ea301df1ac6525c734d30323d63597a48a
Maxim Maslin <zapzarap@yandex.ru>
2017-11-01 19:15:55 +0300

    disregard me

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
+)
