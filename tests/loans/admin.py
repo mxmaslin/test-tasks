100088ca85319da72cc50c1e02073f648cf34c82
Maxim Maslin <zapzarap@yandex.ru>
2017-11-10 17:17:20 +0300

    clean up in loans.admin

--- tests/loans/admin.py
+++ tests/loans/admin.py
@@ -6,90 +6,20 @@ from .models import Questionnaire
 from .models import Submission
 
 
-class ApplicationAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Application
-        fields = '__all__'
-
-
 class ApplicationAdmin(admin.ModelAdmin):
-    form = ApplicationAdminForm
-    list_display = [
-        'created',
-        'modified',
-        'rotation_started',
-        'rotation_ended',
-        'applicaton_name',
-        'application_type',
-        'score_min',
-        'score_max',
-        #'bank'
-        ]
-    readonly_fields = [
-        'created',
-        'modified',
-        'rotation_started',
-        'rotation_ended',
-        'applicaton_name',
-        'application_type',
-        'score_min',
-        'score_max',
-        #'bank'
-        ]
+    pass
+    exclude = ('bank', )
 
 admin.site.register(Application, ApplicationAdmin)
 
 
-class QuestionnaireAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Questionnaire
-        fields = '__all__'
-
-
 class QuestionnaireAdmin(admin.ModelAdmin):
-    form = QuestionnaireAdminForm
-    list_display = [
-        'created',
-        'modified',
-        'name',
-        'birthday',
-        'phone',
-        'passport',
-        'score']
-    readonly_fields = [
-        'created',
-        'modified',
-        'name',
-        'birthday',
-        'phone',
-        'passport',
-        'score']
+    pass
 
 admin.site.register(Questionnaire, QuestionnaireAdmin)
 
 
-class SubmissionAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Submission
-        fields = '__all__'
-
-
 class SubmissionAdmin(admin.ModelAdmin):
-    form = SubmissionAdminForm
-    list_display = [
-        'created',
-        'submitted',
-        'questionnaire',
-        'application',
-        'status']
-    readonly_fields = [
-        'created',
-        'submitted',
-        'questionnaire',
-        'application',
-        'status']
+    pass
 
 admin.site.register(Submission, SubmissionAdmin)
