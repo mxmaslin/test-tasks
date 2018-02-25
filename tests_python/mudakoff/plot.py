603f578cc9c51ca6693634ed84f6285deb713a52
Maxim Maslin <zapzarap@yandex.ru>
2018-02-25 10:50:05 +0300

    add plotly stuff

--- tests_python/mudakoff/plot.py
+++ tests_python/mudakoff/plot.py
@@ -1,40 +1,87 @@
 # -*- coding: utf-8 -*-
-import time
+import time, datetime
+import plotly
+import plotly.graph_objs as go
+import plotly.plotly as py
+import plotly.tools as tls
 import vk_api
 
+plotly.tools.set_credentials_file(username='mxmaslin', api_key='SxjPplGYZgYgbi69kwrc')
 
-def count_stuff(vk):
-    posts_amount = vk.wall.get(domain='mudakoff', count=1)['count']
+
+def count_posts_portion(vk):
+    posts_amount = vk.wall.get(domain='mudakoff')['count']
     offset_counter = 0
-    countdown_start_time = time.ctime()
     likes = 0
     comments = 0
     reposts = 0
     while offset_counter < posts_amount:
-        if posts_amount - offset_counter < 100:
-            offset_counter += posts_amount - offset_counter
-        else:
-            offset_counter += 100
-        for post in vk.wall.get(domain='mudakoff', count=1, offset=offset_counter)['items']:
+        for post in vk.wall.get(domain='mudakoff', count=100, offset=offset_counter)['items']:
             likes += post['likes']['count']
             comments += post['comments']['count']
             reposts += post['reposts']['count']
-    return {'time': countdown_start_time, 'values': [likes, comments, reposts]}
+        offset_counter += 100
+    return likes, comments, reposts
 
 
-def main():
-    login, password = 'mxmaslin@gmail.com', 'q1w2e3r4'
+def set_vk_session():
+    login, password = 'your_login', 'your_password'
     vk_session = vk_api.VkApi(login, password)
-
     try:
         vk_session.auth()
     except vk_api.AuthError as error_msg:
         print(error_msg)
         return
+    return vk_session.get_api()
 
-    vk = vk_session.get_api()
 
-    likes_prev, comments_prev, reposts_prev = count_stuff(vk)['values']
+def main():
+    stream_ids = tls.get_credentials_file()['stream_ids']
+
+    stream_1 = go.Stream(
+        token=stream_ids[0],
+        maxpoints=80)
+    stream_2 = go.Stream(
+        token=stream_ids[1],
+        maxpoints=80)
+    stream_3 = go.Stream(
+        token=stream_ids[2],
+        maxpoints=80)
+
+    trace1 = go.Scatter(
+        x=[],
+        y=[],
+        mode='lines+markers',
+        stream=stream_1,
+        name='Лайки')
+    trace2 = go.Scatter(
+        x=[],
+        y=[],
+        mode='lines+markers',
+        stream=stream_2,
+        name='Комментарии')
+    trace3 = go.Scatter(
+        x=[],
+        y=[],
+        mode='lines+markers',
+        stream=stream_3,
+        name='Репосты')
+
+    data = go.Data([trace1, trace2, trace3])
+
+    layout = go.Layout(title='Изменение количества лайков/комментариев/репостов сообщества http://vk.com/mudakoff')
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='python-streaming')
+
+    s1 = py.Stream(stream_ids[0])
+    s1.open()
+    s2 = py.Stream(stream_ids[1])
+    s2.open()
+    s3 = py.Stream(stream_ids[2])
+    s3.open()
+
+    vk = set_vk_session()
+    likes_prev, comments_prev, reposts_prev = count_posts_portion(vk)
     first_iteration = True
 
     while True:
@@ -42,14 +89,32 @@ def main():
             likes_now, comments_now, reposts_now = likes_prev, comments_prev, reposts_prev
             first_iteration = False
             continue
-        likes_prev, comments_prev, reposts_prev = likes_now, comments_now, reposts_now
-        likes_now, comments_now, reposts_now = count_stuff(vk)['values']
-        likes_change = ((likes_now - likes_prev) / likes_prev) * 100
-        comments_change = ((comments_now - comments_prev) / comments_prev) * 100
-        reposts_change = ((reposts_now - reposts_prev) / reposts_prev) * 100
+        else:
+            likes_prev, comments_prev, reposts_prev = likes_now, comments_now, reposts_now
+            likes_now, comments_now, reposts_now = count_posts_portion(vk)
 
-        print(likes_change, comments_change, reposts_change)
-        print('-')
+            likes_change = likes_now - likes_prev
+            comments_change = comments_now - comments_prev
+            reposts_change = reposts_now - reposts_prev
+
+            x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+            s1.write(dict(x=x, y=likes_change))
+            s2.write(dict(x=x, y=comments_change))
+            s3.write(dict(x=x, y=reposts_change))
+
+            print(x)
+            print(likes_change, likes_now)
+            print(comments_change, comments_now)
+            print(reposts_change, reposts_now)
+            print('')
+
+            time.sleep(1)
+
+    s1.close()
+    s2.close()
+    s3.close()
+    tls.embed('streaming-demos', '12')
 
 
 if __name__ == '__main__':
