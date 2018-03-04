c6446def677ab6ba2d839a9366fdf96ce4b58edd
Maxim Maslin <zapzarap@yandex.ru>
2018-03-04 14:54:00 +0300

    try bokeh

--- tests_python/mudakoff/plot.py
+++ tests_python/mudakoff/plot.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
+from bokeh.plotting import figure, output_file, show
 from functools import reduce
-import plotly
-import plotly.graph_objs as go
-import plotly.plotly as py
-import plotly.tools as tls
 import time, datetime
 import vk_api
-
-plotly.tools.set_credentials_file(username='mxmaslin', api_key='SxjPplGYZgYgbi69kwrc')
 
 
 def count_posts_portion(vk):
@@ -35,52 +30,7 @@ def set_vk_session():
         return
     return vk_session.get_api()
 
-
 def main():
-    stream_ids = tls.get_credentials_file()['stream_ids']
-
-    stream_1 = go.Stream(
-        token=stream_ids[0],
-        maxpoints=80)
-    stream_2 = go.Stream(
-        token=stream_ids[1],
-        maxpoints=80)
-    stream_3 = go.Stream(
-        token=stream_ids[2],
-        maxpoints=80)
-
-    trace1 = go.Scatter(
-        x=[],
-        y=[],
-        mode='lines+markers',
-        stream=stream_1,
-        name='Лайки')
-    trace2 = go.Scatter(
-        x=[],
-        y=[],
-        mode='lines+markers',
-        stream=stream_2,
-        name='Комментарии')
-    trace3 = go.Scatter(
-        x=[],
-        y=[],
-        mode='lines+markers',
-        stream=stream_3,
-        name='Репосты')
-
-    data = go.Data([trace1, trace2, trace3])
-
-    layout = go.Layout(title='Изменение количества лайков/комментариев/репостов сообщества http://vk.com/mudakoff')
-    fig = go.Figure(data=data, layout=layout)
-    py.plot(fig, filename='python-streaming')
-
-    s1 = py.Stream(stream_ids[0])
-    s1.open()
-    s2 = py.Stream(stream_ids[1])
-    s2.open()
-    s3 = py.Stream(stream_ids[2])
-    s3.open()
-
     vk = set_vk_session()
     likes_prev, comments_prev, reposts_prev = count_posts_portion(vk)
     first_iteration = True
@@ -98,24 +48,20 @@ def main():
             comments_change = comments_now - comments_prev
             reposts_change = reposts_now - reposts_prev
 
-            x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            output_file('bokeh_test.html')
+            p = figure(
+            title='bokeh test',
+            x_axis_label='x',
+            y_axis_label='y')
+            datetime.now()
+            p.line(x=datetime.now(),
+                   y=likes_change,
+                   legend='bokeh test',
+                   line_width=2)
 
-            s1.write(dict(x=x, y=likes_change))
-            s2.write(dict(x=x, y=comments_change))
-            s3.write(dict(x=x, y=reposts_change))
 
-            print(x)
-            print(likes_change, likes_now, likes_prev)
-            print(comments_change, comments_now, comments_prev)
-            print(reposts_change, reposts_now, reposts_prev)
-            print('')
 
-            time.sleep(1)
 
-    s1.close()
-    s2.close()
-    s3.close()
-    tls.embed('streaming-demos', '12')
 
 
 if __name__ == '__main__':
