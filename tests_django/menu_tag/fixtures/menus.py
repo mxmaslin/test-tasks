2c27ae163d1d45b6b5404395fbdf4d1955eee9d3
Maxim Maslin <zapzarap@yandex.ru>
2018-03-17 15:50:29 +0300

    add to menu_tag the rest of the tests

--- tests_django/menu_tag/fixtures/menus.py
+++ tests_django/menu_tag/fixtures/menus.py
@@ -1,14 +1,63 @@
-only_top = {'li': [{'': '',
-                    'a': {'': 'меню2 пункт1',
-                          '#href': 'http://testserver/menu_tag/?item=meniu2-punkt1'}
-                    },
-                   {'': '',
-                    'a': {'': 'меню2 пункт2',
-                          '#href': 'http://testserver/menu_tag/?item=meniu2-punkt2'}
-                    },
-                   {'': '',
-                    'a': {'': 'меню2 пункт3',
-                          '#href': 'http://testserver/menu_tag/?item=meniu2-punkt3'}
-                    }
-                   ]
-            }
+absent = {'ul': {'#id': 'meniu0', '': 'Menu meniu0 is absent in DB'}}
+
+empty = {'ul': {'': 'The menu "meniu1" does not contain items', '#id': 'meniu1'}}
+
+no_subbranches = {'ul': {'': '',
+                         '#id': 'meniu2',
+                         'li': [{'': '',
+                                 'a': {'': 'меню2 пункт1',
+                                       '#href': 'http://testserver/menu_tag/?item=meniu2-punkt1'}},
+                                {'': '',
+                                 'a': {'': 'меню2 пункт2',
+                                       '#href': 'http://testserver/menu_tag/?item=meniu2-punkt2'}},
+                                {'': '',
+                                 'a': {'': 'меню2 пункт3',
+                                       '#href': 'http://testserver/menu_tag/?item=meniu2-punkt3'}}]}}
+
+single_subbranch1 = {'ul': {'': '',
+                            '#id': 'meniu3',
+                            'li': [{'': '',
+                                    'a': {'': 'меню3 пункт1',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu3-punkt1'}},
+                                   {'': '',
+                                    'a': {'': 'меню3 пункт2',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu3-punkt2'}},
+                                   {'': '',
+                                    'a': {'': 'меню3 пункт3',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu3-punkt3'}}]}}
+
+single_subbranch2 = {'ul': {'': '',
+                            '#id': 'meniu4',
+                            'li': [{'': '',
+                                    'a': {'': 'меню4 пункт1',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu4-punkt1'}},
+                                   {'': '',
+                                    'a': {'': 'меню4 пункт2',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu4-punkt2'}},
+                                   {'': '',
+                                    'a': {'': 'меню4 пункт3',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu4-punkt3'}}]}}
+
+single_subbranch3 = {'ul': {'': '',
+                            '#id': 'meniu5',
+                            'li': [{'': '',
+                                    'a': {'': 'меню5 пункт1',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu5-punkt1'}},
+                                   {'': '',
+                                    'a': {'': 'меню5 пункт2',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu5-punkt2'}},
+                                   {'': '',
+                                    'a': {'': 'меню5 пункт3',
+                                          '#href': 'http://testserver/menu_tag/?item=meniu5-punkt3'}}]}}
+
+multiple_subbranches = {'ul': {'': '',
+                               '#id': 'meniu6',
+                               'li': [{'': '',
+                                       'a': {'': 'меню6 пункт1',
+                                             '#href': 'http://testserver/menu_tag/?item=meniu6-punkt1'}},
+                                      {'': '',
+                                       'a': {'': 'меню6 пункт2',
+                                             '#href': 'http://testserver/menu_tag/?item=meniu6-punkt2'}},
+                                      {'': '',
+                                       'a': {'': 'меню6 пункт3',
+                                             '#href': 'http://testserver/menu_tag/?item=meniu6-punkt3'}}]}}
--- tests_django/menu_tag/tests.py
+++ tests_django/menu_tag/tests.py
@@ -5,7 +5,14 @@ from django.test import Client, TestCase
 
 from .html_to_json import HTMLtoJSONParser as dictifier
 
-from .fixtures.menus import only_top
+from .fixtures.menus import (absent,
+                             empty,
+                             no_subbranches,
+                             single_subbranch1,
+                             single_subbranch2,
+                             single_subbranch3,
+                             multiple_subbranches)
+
 
 class MenuTagTestCase(TestCase):
     fixtures = ['menu_tag']
@@ -17,9 +24,39 @@ class MenuTagTestCase(TestCase):
         self.soup = BeautifulSoup(html, 'lxml')
 
     def get_menu_content(self, menu_id):
-        return str(self.soup.find('ul', {'id': menu_id}).contents)
+        return str(self.soup.find('ul', {'id': menu_id}))
+
+    def test_absent_menu(self):
+        menu_html = self.get_menu_content('meniu0')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, absent)
+
+    def test_empty_menu(self):
+        menu_html = self.get_menu_content('meniu1')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, empty)
 
     def test_only_top(self):
         menu_html = self.get_menu_content('meniu2')
         menu_dict = dictifier.to_json(menu_html)
-        self.assertEqual(menu_dict, only_top)
+        self.assertEqual(menu_dict, no_subbranches)
+
+    def test_single_subbranch1(self):
+        menu_html = self.get_menu_content('meniu3')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, single_subbranch1)
+
+    def test_single_subbranch2(self):
+        menu_html = self.get_menu_content('meniu4')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, single_subbranch2)
+
+    def test_single_subbranch3(self):
+        menu_html = self.get_menu_content('meniu5')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, single_subbranch3)
+
+    def test_multiple_subbranches(self):
+        menu_html = self.get_menu_content('meniu6')
+        menu_dict = dictifier.to_json(menu_html)
+        self.assertEqual(menu_dict, multiple_subbranches)
