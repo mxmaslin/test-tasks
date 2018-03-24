35f0b498376f5e6d10ee56d402a8f707dda81561
Maxim Maslin <zapzarap@yandex.ru>
2018-03-24 22:38:15 +0300

    implement in portfolio_assets the start/end

--- tests_python/portfolio_assets/calculate.py
+++ tests_python/portfolio_assets/calculate.py
@@ -3,15 +3,18 @@ import os
 import pandas as pd
 
 
-prices_path = os.path.join('data', 'prices.csv')
+def calculate_asset_performance(start_date, end_date):
+    prices_path = os.path.join('data', 'prices.csv')
 
-assets_initial = pd.read_csv(prices_path, header=0)
-assets = assets_initial.fillna(method='ffill')
+    assets = pd.read_csv(prices_path).\
+        fillna(method='ffill').\
+        set_index('date').\
+        loc[start_date:end_date]
 
-assets_t = assets.iloc[1:, 1:].reset_index(drop=True)
-assets_t_minus = assets.iloc[:-1, 1:].reset_index(drop=True)
+    assets_t = assets[1:].reset_index(drop=True)
+    assets_t_minus = assets[:-1].reset_index(drop=True)
+    assets_return = assets_t.subtract(assets_t_minus)
 
-assets_return = assets_t.subtract(assets_t_minus)
+    print(assets_return)
 
-
-print(assets_return)
+calculate_asset_performance('2014-01-13', '2014-01-14')
