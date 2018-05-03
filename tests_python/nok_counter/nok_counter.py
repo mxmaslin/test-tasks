59d9b53644c03c8d87d2bec47ebc8e902f70ec52
Maxim Maslin <zapzarap@yandex.ru>
2018-05-03 23:21:40 +0300

    fix bugs in nok_counter. Need to write asserts for it

--- tests_python/nok_counter/nok_counter.py
+++ tests_python/nok_counter/nok_counter.py
@@ -13,20 +13,20 @@ def count_noks(file):
         for line in f:
             if len(line) > 1:
                 splitted = line.split()
-                time = ' '.join([splitted[0][1:], splitted[1][:-1]])
+                time = '{} {}'.format(splitted[0][1:], splitted[1][:-1])[:-3]
                 event = splitted[-1]
                 if event == 'NOK':
                     nok_times.append(time)
                     if same(nok_times):
                         nok_counter += 1
                     else:
-                        print(nok_counter)
-                        nok_test.append((time, nok_counter))
+                        print(nok_times[0], nok_counter)
+                        nok_test.append((nok_times[0], nok_counter))
                         nok_counter = 1
                         nok_times = [time]
     if len(nok_times) >= 1:
+        print(time, nok_counter)
         nok_test.append((time, nok_counter))
-        print(nok_counter)
     return nok_test
 
 
