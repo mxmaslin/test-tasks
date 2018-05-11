7ef80f0db9431a7aab0568d51a422966fc75db51
Maxim Maslin <zapzarap@yandex.ru>
2018-05-11 16:38:45 +0300

    remove from image_sizer some nonsenses

--- tests_django/image_sizer/models.py
+++ tests_django/image_sizer/models.py
@@ -2,19 +2,17 @@ from django.db import models
 
 
 class Image(models.Model):
-    name = models.CharField(max_length=100)
     file = models.ImageField(upload_to='uploads/', null=True)
-    download_url = models.URLField(null=True, unique=True)
-    jpeg_quality = models.IntegerField(null=True)
+    download_url = models.URLField(null=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.file}'
 
 
 class Resize(models.Model):
     width = models.IntegerField(null=True)
     height = models.IntegerField(null=True)
-    download_url = models.URLField(null=True, unique=True)
+    file = models.ImageField(upload_to='uploads/', null=True)
     image = models.ForeignKey(
         Image, related_name='resizes', on_delete=models.CASCADE, null=True)
 
--- tests_django/image_sizer/views.py
+++ tests_django/image_sizer/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import imghdr
+import os
+
 from django.http import Http404
 
 from rest_framework import status
@@ -12,10 +17,26 @@ from .serializers import ImageSerializer, ResizeSerializer
 @api_view(['POST'])
 def image_create(request):
     """
-    http POST http://127.0.0.1:8000/image-sizer/ <<< '{"name": "shit", "download_url": "http://shit.com", "resizes": []}'
+    http POST http://127.0.0.1:8000/image-sizer/ <<< '{"file": "test", "download_url": "http://image.com/image.jpg", "resizes": [{"width": 100, "height": 150, "format": "jpg", "jpg_quality": 80}, {"width": 100, "height": 150}]}'
     """
     serializer = ImageSerializer(data=request.data)
+    posted = {'file': False, 'download_url': False}
     if serializer.is_valid():
+        if 'file' in request.data:
+            print('got file and maybe url')
+            pass
+        else:
+            print('no file, got url')
+            pass
+
+
+            # image_url = "https://upload.wikimedia.org/wikipedia/ru/thumb/e/ed/Preved.jpg/300px-Preved.jpg"
+        #     resource = urllib.urlopen(data['download_url'])
+        #     image = open.
+
+
+
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
