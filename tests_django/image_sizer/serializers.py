1d4894b45d51452f67cb75f9d406356ce2c92f65
Maxim Maslin <zapzarap@yandex.ru>
2018-05-11 16:38:45 +0300

    remove from image_sizer some nonsenses

--- tests_django/image_sizer/serializers.py
+++ tests_django/image_sizer/serializers.py
@@ -14,48 +14,21 @@ class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Image
-        fields = ('name',
-                  'file',
-                  'download_url',
-                  'jpeg_quality',
-                  'resizes')
+        fields = '__all__'
 
     def validate(self, data):
         """
         Убедимся, что у нас есть либо file, либо download_url
         """
-        file = True
-        download_url = True
-        try:
-            data['file']
-        except KeyError:
-            file = False
-        try:
-            data['download_url']
-        except KeyError:
-            download_url = False
-        if not any([file, download_url]):
-            raise serializers.ValidationError('No file nor url for file download')
-        return data
-
-    def validate_file(self, data):
-        """
-        Убедимся, что file является изображением поддерживаемого типа
-        """
-        try:
-            data['file']
-        except KeyError:
+        if 'file' in data or 'download_url' in data:
             pass
         else:
-            if imghdr.what not in ('png', 'gif', 'jpg'):
-                raise serializers.ValidationError('Unsupported file format')
+            raise serializers.ValidationError('No file nor url for file download')
         return data
 
 
 class ResizeSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Resize
-        fields = ('width',
-                  'height',
-                  'download_url',
-                  'image')
+        fields = '__all__'
