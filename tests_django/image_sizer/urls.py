c1d65b10fdde351e49db9244d1a10068decf6a42
Maxim Maslin <zapzarap@yandex.ru>
2018-05-13 10:35:44 +0300

    add to image_sizer.urls the regex for getting resizes

--- tests_django/image_sizer/urls.py
+++ tests_django/image_sizer/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.image_create),
     re_path(
-        r'(?P<path>)/',
+        r'^(?P<path>media\/images\/\d{4}\/\d{2}\/\d{2}\/[a-zA-Z0-9_-]+(.jpg|.JPG|.gif|.GIF|.png|.PNG)*)$',
         views.ResizeDetail.as_view(), name='resize-detail')
 ]
