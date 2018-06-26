96133022dcf68f9a973507b02097959427d4b991
Maxim Maslin <zapzarap@yandex.ru>
2018-06-26 20:04:02 +0300

    fix minor bug

--- tests_django/playschool/urls.py
+++ tests_django/playschool/urls.py
@@ -7,12 +7,12 @@ from . import views
 urlpatterns = [
 
     # Создать ребёнка
-    re_path(r'^children/$', views.ScholarCreateAPIView.as_view()),
+    re_path(r'^scholars/$', views.ScholarCreateAPIView.as_view()),
 
     # Создать отметку в журнал
-    re_path(r'^records/$', views.RecordCreateAPIView),
+    re_path(r'^records/$', views.RecordCreateAPIView.as_view()),
     # Изменить отметку в журнале
-    path('records/<int:pk>/', views.RecordUpdateAPIView),
+    path('records/<int:pk>/', views.RecordUpdateAPIView.as_view()),
     # Список тех, кто сейчас учится
     path('records/studying/', views.studying_scholars)
 ]
