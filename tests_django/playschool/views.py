3a0935d66760af3ce4e11c61f42a8dd8bd73b246
Maxim Maslin <zapzarap@yandex.ru>
2018-06-26 19:05:17 +0300

    add views

--- tests_django/playschool/views.py
+++ tests_django/playschool/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.response import Response
+from .models import Scholar, Record
+from .serializers import ScholarSerializer, RecordSerializer
 
-# Create your views here.
+
+class ScholarCreateAPIView(CreateAPIView):
+    queryset = Scholar.objects.all()
+    serializer_class = ScholarSerializer
+
+
+class RecordCreateAPIView(CreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+
+class RecordUpdateAPIView(UpdateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+
+@api_view(['GET'])
+def studying_scholars(request):
+    are_studying = Scholar.studying.all()
+    serializer = ScholarSerializer(are_studying, many=True)
+    return Response(serializer.data)
