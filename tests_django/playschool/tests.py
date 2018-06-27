497c02bf886ef34fcb81f7dc1d23643695476351
Maxim Maslin <zapzarap@yandex.ru>
2018-06-27 08:06:18 +0300

    add some to tests

--- tests_django/playschool/tests.py
+++ tests_django/playschool/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class ScholarAPITestCase(TestCase):
+    # fixtures = ['playschool']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_create(self):
+        response = self.client.post(
+            '/playschool/scholars/',
+            {
+                'photo': '?',
+                'name': 'Vasiliy',
+                'sex': 'M',
+                'birth_date': '2018-1-31',
+                'school_class': 1,
+                'is_studying': True
+            })
+        print(response.reason_phrase)
+        self.assertEqual(response.status_code, 201)
