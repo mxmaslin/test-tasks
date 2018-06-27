cb9c0784ea611fa73564d91c2b2417db59ebbf83
Maxim Maslin <zapzarap@yandex.ru>
2018-06-27 13:03:57 +0300

    add admin

--- tests_django/playschool/admin.py
+++ tests_django/playschool/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Scholar, Record
+
+
+@admin.register(Scholar)
+class ScholarAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    pass
