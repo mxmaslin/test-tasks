aec8a5ac8a03c4f49c1f02c10ca395685735678c
Maxim Maslin <zapzarap@yandex.ru>
2018-09-24 20:43:43 +0300

    add gen_sum python task

---
+++ tests_python/gen_sum/gen_sum.py
@@ -0,0 +1,25 @@
+from itertools import chain
+
+
+src_a1 = ({k: v} for k, v in zip(['a', 'b', 'c'], [0, 1, 2]))
+src_b1 = ({k: v} for k, v in zip(['b', 'c'], [1, 2]))
+src_c1 = ({k: v} for k, v in zip(['c'], [2]))
+
+src_a2 = ({k: v} for k, v in zip(['a', 'b', 'c'], [2, 1, 0]))
+src_b2 = ({k: v} for k, v in zip(['a', 'b'], [2, 1]))
+src_c2 = ({k: v} for k, v in zip(['a'], [2]))
+
+
+def gen_sum(src_a, src_b, src_c):
+    result = dict()
+    for d in chain(src_a, src_b, src_c):
+        k, v = zip(*d.items())
+        k, v = k[0], v[0]
+        cur_val = result.get(k, 0)
+        result[k] = cur_val + v
+    return result
+
+
+if __name__ == '__main__':
+    assert gen_sum(src_a1, src_b1, src_c1) == {'a': 0, 'b': 2, 'c': 6}
+    assert gen_sum(src_a2, src_b2, src_c2) == {'a': 6, 'b': 2, 'c': 0}
