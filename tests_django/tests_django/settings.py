cb7342ed64d8a8b80bbfa6706d53d7680d8f9d2d
Maxim Maslin <zapzarap@yandex.ru>
2018-09-30 11:10:58 +0300

    remove admin-view-permissions

--- tests_django/tests_django/settings.py
+++ tests_django/tests_django/settings.py
@@ -42,7 +42,6 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_view_permission',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
