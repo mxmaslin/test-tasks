ababbf65eea1e63c43d43fe62d745a9a01d3199b
Maxim Maslin <zapzarap@yandex.ru>
2019-01-21 10:37:51 +0300

    add admin for gen-keys

--- tests_django/apps/gen_keys/admin.py
+++ tests_django/apps/gen_keys/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Key
+
+
+@admin.register(Key)
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ('value', 'is_provided', 'is_expired')
