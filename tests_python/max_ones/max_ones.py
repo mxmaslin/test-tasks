7bfa1f74dae5fb31188a69e3989ecc8a4d1a2301
Maxim Maslin <zapzarap@yandex.ru>
2019-03-25 21:25:17 +0300

    simlify the code, add assert

--- tests_python/max_ones/max_ones.py
+++ tests_python/max_ones/max_ones.py
@@ -1,12 +1,12 @@
 def max_ones(int_list):
-    if len(int_list) < 3:
-        return sum(int_list)
     str_list = ''.join(map(str, int_list)).split('0')
+    if len(int_list) < 3 or len(str_list) == 1:
+        return sum(int_list)
     calculated_max_ones = 0
     for i in range(0, len(str_list) - 1):
         first, second = str_list[i], str_list[i + 1]
-        first = sum([int(x) for x in first]) if first else 0
-        second = sum([int(x) for x in second]) if second else 0
+        first = len(first) if first else 0
+        second = len(second) if second else 0
         pair_sum = first + second
         if pair_sum > calculated_max_ones:
             calculated_max_ones = pair_sum
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     assert max_ones([1, 1]) == 2
     assert max_ones([1, 0, 1, 0]) == 2
     assert max_ones([0, 1, 0, 1]) == 2
+    assert max_ones([1, 1, 1, 1, 1]) == 5
+    assert max_ones([0, 0, 0, 0, 0]) == 0
     assert max_ones([1, 0, 0, 1, 0]) == 1
     assert max_ones([0, 1, 1, 0, 1]) == 3
     assert max_ones([1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1]) == 6
