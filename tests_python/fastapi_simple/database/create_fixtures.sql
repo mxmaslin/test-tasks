5919f0d4559b6cd390edb64a70842896c530c4f1
Maxim Maslin <zapzarap@yandex.ru>
2021-11-25 13:31:39 +0300

    fastapi simple fixtures

---
+++ tests_python/fastapi_simple/database/create_fixtures.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS persons (
+    first_name VARCHAR(20),
+    patronymic VARCHAR(20),
+    last_name VARCHAR(20),
+    sex CHAR,
+    phone VARCHAR(11),
+    country VARCHAR(20)
+);
