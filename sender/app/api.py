bc41d9a9db967e75f0a85e4662e1c7e581306505
Maxim Maslin <mmaslin@contractors.sk.ru>
2022-12-19 15:45:41 +0300

    Implement updating recipient

--- sender/app/api.py
+++ sender/app/api.py
@@ -31,8 +31,50 @@ def add_recipient(body: RequestRecipientModel):
 
             db_tags_ids = [tag.id for tag in db_tags]
             tag_recipients = [
-                TagRecipient(tag=tag, recipient=recipient.id)
-                for tag in db_tags_ids
+                TagRecipient(tag=tag_id, recipient=recipient.id)
+                for tag_id in db_tags_ids
+            ]
+            TagRecipient.bulk_create(tag_recipients)
+        except Exception as e:
+            tx.rollback()
+            logger.error(str(e))
+            return str(e), 400
+
+        tx.commit()
+        return 'Success', 200
+
+
+
+@app.route(f'/{PREFIX}/recipients/<int:recipient_id>', methods=['PUT'])
+@validate()
+def update_recipient(recipient_id: int, body: RequestRecipientModel):
+
+    with db.atomic() as tx:
+        try:
+            phone_number = body.phone_number
+            op_code = body.op_code
+            tz = body.tz
+            query = Recipient.update(
+                phone_number=phone_number, op_code=op_code, tz=tz
+            ).where(Recipient.id==recipient_id)
+            query.execute()
+    
+            tags = body.tags
+            tag_recipients = TagRecipient.select().join(Tag).where(
+                TagRecipient.recipient==recipient_id,
+                Tag.value.in_(tags)
+
+            ).execute()
+            tags_to_create = list(
+                set(tags) - {x.tag.value for x in tag_recipients}
+            )
+            tags_to_create = [Tag(value=tag) for tag in tags_to_create]
+            Tag.bulk_create(tags_to_create)
+
+            tags_to_create_ids = [tag.id for tag in tags_to_create]
+            tag_recipients = [
+                TagRecipient(tag=tag_id, recipient=recipient_id)
+                for tag_id in tags_to_create_ids
             ]
             TagRecipient.bulk_create(tag_recipients)
         except Exception as e:
