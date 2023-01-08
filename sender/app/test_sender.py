1a9fd0727d6f789c2ee8581e8bbb27c12b762d8e
Maxim Maslin <mmaslin@itkey.com>
2023-01-08 16:33:45 +0300

    fix with_test_db

--- sender/app/test_sender.py
+++ sender/app/test_sender.py
@@ -5,22 +5,21 @@ from playhouse.sqlite_ext import SqliteExtDatabase
 from tasks import get_periodic_messages_to_send
 
 
-def with_test_db(models: tuple):
-    def decorator(func):
-        @wraps(func)
-        def test_db_closure(*args, **kwargs):
-            test_db = SqliteExtDatabase(":memory:")
-            with test_db.bind_ctx(models):
-                test_db.create_tables(models)
-                try:
-                    func(*args, **kwargs)
-                finally:
-                    test_db.drop_tables(models)
-                    test_db.close()
+def with_test_db(func):
+    @wraps(func)
+    def test_db_closure(*args, **kwargs):
+        test_db = SqliteExtDatabase(':memory:')
+        models = ()
+        with test_db.bind_ctx(models):
+            test_db.create_tables(models)
+            test_db.execute_sql()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                test_db.drop_tables(models)
+                test_db.close()
 
-        return test_db_closure
-
-    return decorator
+    return test_db_closure
 
 
 def test_get_periodic_messages_to_send():
