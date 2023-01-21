fe09c8db088e6366b178a13d30ee54df48f166da
Maxim Maslin <mmaslin@itkey.com>
2023-01-21 17:56:51 +0300

    refactor logger

--- social/app/logger.py
+++ social/app/logger.py
@@ -1,16 +1,20 @@
 import logging
 
-logger = logging.getLogger('booking')
-logger.setLevel(logging.DEBUG)
+from pathlib import Path
 
-fh = logging.FileHandler('error.log')
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    """%(levelname)s in %(module)s [%(pathname)s:%(lineno)d]:\n%(message)s"""
+
+file_name = Path.cwd().joinpath('error.log')
+
+logging.basicConfig(
+    filename=file_name,
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
-fh.setFormatter(formatter)
+logger = logging.getLogger('social')
+
+fh = logging.FileHandler(file_name)
 logger.addHandler(fh)
 
 ch = logging.StreamHandler()
-ch.setFormatter(formatter)
 logger.addHandler(ch)
