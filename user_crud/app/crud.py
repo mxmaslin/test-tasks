aeadc0cc9c50b858119ed1bca60450d6d870e457
Maxim Maslin <mmaslin@itkey.com>
2023-02-10 16:10:55 +0300

    implement crud

--- user_crud/app/crud.py
+++ user_crud/app/crud.py
@@ -1,93 +1,22 @@
 from sqlalchemy.orm import Session
 
-from models import User, Post, Like, Dislike
-from schemas import PostModel
+from app.models import User
+from app.schemas import UserCreate
 
 
-def get_user(db: Session, email: str) -> User:
-    return db.query(User).filter(User.email==email).first()
+def get_user(db: Session, phone_number: str) -> User:
+    return db.query(User).filter(User.phone_number==phone_number).first()
 
 
-def create_user(db: Session, email: str, password_hash: str):
-    user = User(email=email, password_hash=password_hash)
+def create_user(db: Session, data: UserCreate):
+    user = User(**data)
     db.add(user)
     db.commit()
     db.refresh(user)
     return user
 
 
-def get_post(db: Session, post_id: int) -> Post:
-    return db.query(Post).filter(Post.id==post_id).first()
-
-
-def get_user_post(db: Session, post_id: int):
-    return db.query(Post).join(
-        User,
-        User.id == Post.user_id
-    ).filter(Post.id==post_id).first()
-
-
-def get_posts(db: Session, limit: int = 0, offset: int = 0):
-    return db.query(Post).filter().limit(limit).offset(offset)
-
-
-def create_post(db: Session, user_id: int, title: str, content: str):
-    post = Post(user_id=user_id, title=title, content=content)
-    db.add(post)
+def delete_user(db: Session, phone_number: str):
+    user = get_user(db, phone_number)
+    db.delete(user)
     db.commit()
-    db.refresh(post)
-    return post
-
-
-def update_post(
-    db: Session,
-    post: Post,
-    post_new_data: PostModel
-):
-    new_data = post_new_data.dict(exclude_unset=True)
-    for k, v in new_data.items():
-        setattr(post, k, v)
-    db.add(post)
-    db.commit()
-    db.refresh(post)
-    return post
-
-
-def delete_post(db: Session, post_id: int):
-    post = get_post(db, post_id)
-    db.delete(post)
-    db.commit()
-
-
-def delete_like(db: Session, post_id: int, user_id: int):
-    like = db.query(Like).filter(
-        Like.post_id==post_id, Like.user_id==user_id
-    ).first()
-    if like is not None:
-        db.delete(like)
-        db.commit()
-
-
-def delete_dislike(db: Session, post_id: int, user_id: int):
-    dislike = db.query(Dislike).filter(
-        Dislike.post_id==post_id, Dislike.user_id==user_id
-    ).first()
-    if dislike is not None:
-        db.delete(dislike)
-        db.commit()
-
-
-def create_like(db: Session, post_id: int, user_id: int):
-    like = Like(post_id=post_id, user_id=user_id)
-    db.add(like)
-    db.commit()
-    db.refresh(like)
-    return like
-
-
-def create_dislike(db: Session, post_id: int, user_id: int):
-    dislike = Dislike(post_id=post_id, user_id=user_id)
-    db.add(dislike)
-    db.commit()
-    db.refresh(dislike)
-    return dislike
