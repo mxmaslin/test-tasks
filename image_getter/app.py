9e5d5e7e059470730ed5dfe60baab30e0da073b3
Maxim Maslin <mmaslin@contractors.sk.ru>
2023-04-07 12:03:31 +0300

    image getter

--- image_getter/app.py
+++ image_getter/app.py
@@ -1,6 +1,10 @@
 import csv
+import json
+import random
+from functools import lru_cache
+from multiprocessing import Value
 from typing import List
-from flask import Flask, request
+from flask import Flask, request, Response
 
 
 app = Flask(__name__)
@@ -14,14 +18,38 @@ def config_to_list(filepath: str) -> List:
             entries.append(entry)
     return entries
 
-config = config_to_list('config.csv')
+
+@lru_cache
+def get_list_config(filename):
+    return config_to_list(filename)
 
 
 @app.route('/', methods=['GET'])
 def get_image():
+    config = get_list_config('config.csv')
     categories = request.args.getlist('category')
-    print(config)
-    return '200'
+    if not categories:
+        random_entry = random.choice(config)
+        _, _, *categories = random_entry
+
+    category = random.choice(categories)
+    for entry in config:
+        image_url, display_counter, *entry_categories = entry
+        display_counter = Value('i', int(display_counter))
+        if category in entry_categories and display_counter.value > 0:
+            with display_counter.get_lock():
+                display_counter.value -= 1
+            entry[1] = display_counter
+            random.shuffle(config)
+            resp = {
+                'success': True, 'image': f'<img src="{image_url}">'
+            }
+            return Response(
+                json.dumps(resp), status=200, mimetype='application/json'
+            )
+
+    resp = {'success': False, 'image': None}
+    return Response(json.dumps(resp), status=400, mimetype='application/json')
 
 
 if __name__ == '__main__':
--- image_getter/prepare_config.py
+++ image_getter/prepare_config.py
@@ -12,13 +12,13 @@ image_entries = []
 
 if __name__ == '__main__':
     filename = 'config.csv' 
-    with open(filename, 'a') as csvfile:
+    with open(filename, 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=';', quotechar='\n')
         while counter < 1000:
             image_url = fake.file_name(category='image')
             if image_url not in image_urls:
                 image_urls.add(image_url)
                 counter += 1
-            display_counter = str(fake.random_int(1, 10))
-            categories = [fake.word() for _ in range(fake.random_int(1, 10))]
-            writer.writerow([image_url, display_counter, *categories])
+                display_counter = str(fake.random_int(1, 10))
+                categories = [fake.word() for _ in range(fake.random_int(1, 10))]
+                writer.writerow([image_url, display_counter, *categories])
