330199650b666ad31d4c383339997ebcff7ecce6
Маслин Максим Михайлович <m.maslin@samolet.ru>
2024-08-21 09:59:47 +0300

    add models

--- easy_xml/app/easy_xml/models.py
+++ easy_xml/app/easy_xml/models.py
@@ -1,3 +1,50 @@
+import datetime
+from decimal import Decimal
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, help_text='Название продукта')
+
+    class Meta:
+        ordering = ['name',]
+
+    def __str__(self):
+        return self.name
+
+
+class Tariff(models.Model):
+    name = models.CharField(max_length=255, help_text='Название тарифа')
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.01'))],
+        help_text='Стоимость',
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, help_text='Продукт')
+
+    class Meta:
+        ordering = ['name', '-price']
+
+    def __str__(self):
+        return self.name
+
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=255, help_text='Название акции')
+    discount = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)],
+        help_text='Процент скидки',
+    )
+    start_at = models.DateField(help_text='Дата начала акции')
+    end_at = models.DateField(help_text='Дата завершения акции')
+    tariffs = models.ManyToManyField(Tariff)
+
+    class Meta:
+        ordering = ['name', '-discount']
+
+    def __str__(self):
+        return self.name
