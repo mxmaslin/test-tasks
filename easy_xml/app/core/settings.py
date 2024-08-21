ee3791abd06493c83060d43c510e18cd6141393a
Маслин Максим Михайлович <m.maslin@samolet.ru>
2024-08-21 14:50:13 +0300

    dockerize

--- easy_xml/app/core/settings.py
+++ easy_xml/app/core/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'easy_xml',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
